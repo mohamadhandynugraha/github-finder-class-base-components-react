182871e151a82bce5173334939e21e2f663cbc84
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-04-29 07:48:05 +0700

    section 9 belajar condition dan expression

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render () {
-    return (
+  iniContohMethodDariClassApp = () => "Test Method";
+
+  render() {
+    const name = "John Wall"; // contoh buat variable
+    const showName = false;
+    const isLoading = true;
+
+    const iniContohMethodDidalamRender = () => `1 + 1 = ${1 + 1}`;
+    // ini contoh function biasa bukan arrow
+    function testMethodDalamRender(a, b) {
+      return a + b;
+    }
+
+    return isLoading ? (
       <>
-        <h1>Hello World!</h1>
-        <h2>Goodbye!</h2>
+        <h1>Please Wait...</h1>
+      </>
+    ) : (
+      <>
+        <h1>Hello {showName && name.toUpperCase()}!</h1>
+        <h2>{this.iniContohMethodDariClassApp()}</h2>
+        <h3>Panggil method didalam render: {iniContohMethodDidalamRender()}</h3>
+        <h4>Panggil method function biasa: {testMethodDalamRender(5, 5)}</h4>
       </>
     );
   }
