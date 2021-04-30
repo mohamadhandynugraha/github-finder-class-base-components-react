60e2b163f3cc47e8c85d0e2a33bfc0130acc8141
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-04-30 07:51:20 +0700

    section 10 belajar class base component, props and prop types

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar"
 import "./App.css";
 
 class App extends Component {
-  iniContohMethodDariClassApp = () => "Test Method";
+  // iniContohMethodDariClassApp = () => "Test Method";
 
-  render() {
-    const name = "John Wall"; // contoh buat variable
-    const showName = false;
-    const isLoading = true;
+  // render() {
+  //   const name = "John Wall"; // contoh buat variable
+  //   const showName = false;
+  //   const isLoading = true;
 
-    const iniContohMethodDidalamRender = () => `1 + 1 = ${1 + 1}`;
-    // ini contoh function biasa bukan arrow
-    function testMethodDalamRender(a, b) {
-      return a + b;
+  //   const iniContohMethodDidalamRender = () => `1 + 1 = ${1 + 1}`;
+  //   // ini contoh function biasa bukan arrow
+  //   function testMethodDalamRender(a, b) {
+  //     return a + b;
+  //   }
+
+  //   return isLoading ? (
+  //     <>
+  //       <h1>Please Wait...</h1>
+  //     </>
+  //   ) : (
+  //     <>
+  //       <h1>Hello {showName && name.toUpperCase()}!</h1>
+  //       <h2>{this.iniContohMethodDariClassApp()}</h2>
+  //       <h3>Panggil method didalam render: {iniContohMethodDidalamRender()}</h3>
+  //       <h4>Panggil method function biasa: {testMethodDalamRender(5, 5)}</h4>
+  //     </>
+  //   );
+    render () {
+      return (
+        <div className="App">
+          <Navbar />
+        </div>
+      )
     }
-
-    return isLoading ? (
-      <>
-        <h1>Please Wait...</h1>
-      </>
-    ) : (
-      <>
-        <h1>Hello {showName && name.toUpperCase()}!</h1>
-        <h2>{this.iniContohMethodDariClassApp()}</h2>
-        <h3>Panggil method didalam render: {iniContohMethodDidalamRender()}</h3>
-        <h4>Panggil method function biasa: {testMethodDalamRender(5, 5)}</h4>
-      </>
-    );
   }
-}
 
 export default App;
